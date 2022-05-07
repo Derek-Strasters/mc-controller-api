bde6a10326b3e3386bf8ca572596af063f149fdc
derek-strasters <paracite.org@gmail.com>
2022-05-07 03:09:08 -0600

    added status endpoint and root gives version

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import os
+from pathlib import Path
+
+import tomli
+
 from fastapi import FastAPI
-from docker import client
+from docker import DockerClient
+
+MC_SERVER_NAME = os.environ.get("MC_SERVER_NAME", "mc-server")
 
 app = FastAPI()
+docker = DockerClient(base_url="unix://var/run/docker.sock")
+
+with open(Path("pyproject.toml"), "rb") as pyproject:
+    __version__ = tomli.load(pyproject).get("tool.poetry", {}).get("version", "0.0.0")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"version": __version__}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/status/")
+async def say_hello(server_name: str):
+    return {"status": docker.containers.get(MC_SERVER_NAME).status}
