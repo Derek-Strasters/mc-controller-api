6b4c6d4c6600cee1b8f343a541c8d9c23528c48c
derek.strasters <derek.strasters@vivint.com>
2022-05-10 19:09:40 -0600

    adding models

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-FROM python:3.9
+FROM python:3.10.4
 
 WORKDIR /code
 
 COPY ./requirements.txt /code/requirements.txt
+COPY ./pyproject.toml /code/pyproject.toml
 
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+RUN pip install --no-cache-dir -r /code/requirements.txt
+
+COPY ./main.py /code/main.py
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 """Endpoints to control a Minecraft Bedrock server running in a docker container."""
+from __future__ import annotations
+
 import json
 import os
-from pathlib import Path
+import pathlib
+from enum import Enum
+from typing import List
+from uuid import UUID
 
 import docker as docker_package
-import fastapi
 import tomli
+from fastapi import Body, FastAPI, HTTPException, Response, status
+from pydantic import BaseModel, Field, conlist
 
 MC_SERVER_NAME = os.environ.get("MC_DOCKER_NAME", "mc-server")
 DOCKER_BASE_URL = os.environ.get("DOCKER_BASE_URL", "unix://var/run/docker.sock")
 
-app = fastapi.FastAPI()
+app = FastAPI()
 docker = docker_package.DockerClient(base_url=DOCKER_BASE_URL)
-MC_ROOT = Path("/data")
+MC_ROOT = pathlib.Path("/data")
 LEVELS_DIR = MC_ROOT / "worlds"
 
 __version__ = None
@@ -22,7 +28,7 @@ __version__ = None
 def startup_event():
     """Single source the version number from the pyproject.toml file."""
     global __version__
-    pyproject_path = Path("pyproject.toml")
+    pyproject_path = pathlib.Path("pyproject.toml")
     if pyproject_path.is_file():
         with pyproject_path.open("rb") as pyproject:
             __version__ = tomli.load(pyproject).get("tool", {}).get("poetry", {}).get("version", "???")
@@ -32,25 +38,48 @@ def startup_event():
         __version__ = "???"
 
 
-@app.get("/")
+class Version(BaseModel):
+    version: int
+
+
+@app.get("/", response_model=Version)
 async def root():
     """Return the version of the API."""
-    return {"version": __version__}
+    ver = Version()
+    ver.version = __version__
+    return ver
 
 
-@app.get("/status/")
-def status():
-    """Return whether the server is running or not."""
-    return {"status": docker.containers.get(MC_SERVER_NAME).status}
+class Behavior_Pack(BaseModel):
+    name: str | None
+    can_be_redownloaded: bool | None
+    uuid: UUID
+    version: conlist(int, min_items=3, max_items=3)
 
 
-@app.get("/levels/{level_name}")
-def levels(level_name: str = None):
-    """Return a list of levels or details of a level."""
-    if level_name is None:
-        level_list = list(level.name for level in LEVELS_DIR.iterdir())
-        return {"levels": level_list}
+class Resource_Pack(BaseModel):
+    name: str | None = None
+    can_be_redownloaded: bool | None = None
+    uuid: UUID
+    version: conlist(int, min_items=3, max_items=3)
 
+
+class Level(BaseModel):
+    name: str
+    behavior_packs: List[Behavior_Pack] | None
+    resource_packs: List[Resource_Pack] | None
+
+
+@app.get("/levels/", response_model=List[Level], response_model_exclude_unset=True)
+def read_levels():
+    """Return a list of levels."""
+    level_list: List[Level] = list(read_level(level.name) for level in LEVELS_DIR.iterdir())
+    return level_list
+
+
+@app.get("/level/{level_name}", response_model=Level, response_model_exclude_unset=True)
+def read_level(level_name: str = None):
+    """Return the details of a level."""
     level_dir = LEVELS_DIR / level_name
     if level_dir.is_dir():
         behavior_packs = []
@@ -64,18 +93,50 @@ def levels(level_name: str = None):
             with rp_list_path.open() as rp_file:
                 resource_packs = json.load(rp_file)
 
-        return {
-            "level": {
-                "name": level_name,
-                "behavior_packs": behavior_packs,
-                "resource_packs": resource_packs,
-            },
-        }
+        level: Level = Level(name=level_name, behavior_packs=behavior_packs, resource_packs=resource_packs)
 
-    raise fastapi.HTTPException(status_code=404, detail="That level was not fu*king found!")
+        return level
+
+    raise HTTPException(status_code=404, detail="That level was not fu*king found!")
 
 
-@app.post("/controller/")
-def controller():
+class Actions(str, Enum):
+    start = "start"
+    stop = "stop"
+    restart = "restart"
+
+
+class Control(BaseModel):
+    action: Actions
+    message: str | None = None
+
+
+@app.post("/control/", response_model=Control)
+def create_control(response: Response, control: Control = Body(..., embed=True)):
     """Issue control commands to the Minecraft server."""
-    pass
+    container = docker.containers.get(MC_SERVER_NAME)
+
+    # Get the desired method for the MC server docker container action and execute.
+    action = getattr(container, control.action)
+    if callable(action):
+        action()
+        response.status_code = status.HTTP_202_ACCEPTED
+        return control
+    else:
+        control.message = action
+        return control
+
+
+class Statuses(str, Enum):
+    running = "running"
+    exited = "exited"
+
+
+class Status(BaseModel):
+    status: Statuses
+
+
+@app.get("/status/", response_model=Status)
+def read_status():
+    """Return whether the server is running or not."""
+    return Status(status=docker.containers.get(MC_SERVER_NAME).status)
