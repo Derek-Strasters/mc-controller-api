da395c09fc73a881ea012b804d73a171c3d11ab3
derek-strasters <paracite.org@gmail.com>
2022-05-11 23:18:23 -0600

    implemented some endpoints

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import json
 import os
 import pathlib
 from enum import Enum
-from typing import List
 from uuid import UUID
 
 import docker as docker_package
 import tomli
-from fastapi import Body, FastAPI, HTTPException, Response, status
-from pydantic import BaseModel, Field, conlist
+from fastapi import Body, FastAPI, HTTPException
+from pydantic import BaseModel, conlist
 
 MC_SERVER_NAME = os.environ.get("MC_DOCKER_NAME", "mc-server")
 DOCKER_BASE_URL = os.environ.get("DOCKER_BASE_URL", "unix://var/run/docker.sock")
@@ -32,7 +31,7 @@ def startup_event():
     if pyproject_path.is_file():
         with pyproject_path.open("rb") as pyproject:
             __version__ = tomli.load(pyproject).get("tool", {}).get("poetry", {}).get("version", "???")
-            # TODO: make a log
+            # TODO: # TODO Make a log
             print(f"Version {__version__}")
     else:
         __version__ = "???"
@@ -42,7 +41,10 @@ class Version(BaseModel):
     version: int
 
 
-@app.get("/", response_model=Version)
+@app.get(
+    "/",
+    response_model=Version,
+)
 async def root():
     """Return the version of the API."""
     ver = Version()
@@ -50,14 +52,17 @@ async def root():
     return ver
 
 
-class Behavior_Pack(BaseModel):
-    name: str | None
-    can_be_redownloaded: bool | None
+# --- --- --- --- --- --- --- --- --- ---
+
+
+class BehaviorPack(BaseModel):
+    name: str | None = None
+    can_be_redownloaded: bool | None = None
     uuid: UUID
     version: conlist(int, min_items=3, max_items=3)
 
 
-class Resource_Pack(BaseModel):
+class ResourcePack(BaseModel):
     name: str | None = None
     can_be_redownloaded: bool | None = None
     uuid: UUID
@@ -66,65 +71,84 @@ class Resource_Pack(BaseModel):
 
 class Level(BaseModel):
     name: str
-    behavior_packs: List[Behavior_Pack] | None
-    resource_packs: List[Resource_Pack] | None
+    behavior_packs: list[BehaviorPack] | None = None
+    resource_packs: list[ResourcePack] | None = None
 
 
-@app.get("/levels/", response_model=List[Level], response_model_exclude_unset=True)
+@app.get(
+    "/levels/",
+    response_model=list[Level],
+    response_model_exclude_unset=True,
+)
 def read_levels():
     """Return a list of levels."""
-    level_list: List[Level] = list(read_level(level.name) for level in LEVELS_DIR.iterdir())
+    level_list: list[Level] = list(read_level(level.name) for level in LEVELS_DIR.iterdir())
     return level_list
 
 
-@app.get("/level/{level_name}", response_model=Level, response_model_exclude_unset=True)
-def read_level(level_name: str = None):
+@app.get(
+    "/level/{level_name}",
+    response_model=Level,
+    response_model_exclude_unset=True,
+)
+def read_level(level_name: str):
     """Return the details of a level."""
     level_dir = LEVELS_DIR / level_name
     if level_dir.is_dir():
-        behavior_packs = []
-        resource_packs = []
-        bp_list_path = level_dir / "world_behavior_packs.json"
-        rp_list_path = level_dir / "world_resource_packs.json"
-        if bp_list_path.is_file():
-            with bp_list_path.open() as bp_file:
-                behavior_packs = json.load(bp_file)
-        if rp_list_path.is_file():
-            with rp_list_path.open() as rp_file:
-                resource_packs = json.load(rp_file)
+        level: Level = Level(name=level_name)
 
-        level: Level = Level(name=level_name, behavior_packs=behavior_packs, resource_packs=resource_packs)
+        if (level_dir / "world_behavior_packs.json").is_file():
+            with (level_dir / "world_behavior_packs.json").open() as bp_file:
+                level.behavior_packs = json.load(bp_file)
+
+        if (level_dir / "world_resource_packs.json").is_file():
+            with (level_dir / "world_resource_packs.json").open() as rp_file:
+                level.resource_packs = json.load(rp_file)
 
         return level
 
     raise HTTPException(status_code=404, detail="That level was not fu*king found!")
 
 
+@app.get(
+    "/current_level/",
+    response_model=Level,
+    response_model_exclude_unset=True,
+)
+def read_current_level():
+    """Return the current level."""
+    # TODO: # TODO STUB
+    pass
+
+
+@app.put(
+    "/current_level/",
+    response_model=Level,
+    response_model_exclude_unset=True,
+)
+def update_current_level():
+    """Set the current level."""
+    # TODO: # TODO STUB
+    pass
+
+
+# --- --- --- --- --- --- --- --- --- ---
+
+
 class Actions(str, Enum):
     start = "start"
     stop = "stop"
     restart = "restart"
+    status = "status"
 
 
-class Control(BaseModel):
+class ControlOut(BaseModel):
     action: Actions
     message: str | None = None
 
 
-@app.post("/control/", response_model=Control)
-def create_control(response: Response, control: Control = Body(..., embed=True)):
-    """Issue control commands to the Minecraft server."""
-    container = docker.containers.get(MC_SERVER_NAME)
-
-    # Get the desired method for the MC server docker container action and execute.
-    action = getattr(container, control.action)
-    if callable(action):
-        action()
-        response.status_code = status.HTTP_202_ACCEPTED
-        return control
-    else:
-        control.message = action
-        return control
+class ControlIn(BaseModel):
+    action: Actions
 
 
 class Statuses(str, Enum):
@@ -136,7 +160,31 @@ class Status(BaseModel):
     status: Statuses
 
 
-@app.get("/status/", response_model=Status)
+@app.post(
+    "/control/",
+    response_model=ControlOut,
+    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
+)
+def create_control(control_in: ControlIn = Body(..., embed=True)):
+    """Issue control commands to the Minecraft server."""
+    container = docker.containers.get(MC_SERVER_NAME)
+
+    # Get the desired method for the MC server docker container action and execute.
+    action = getattr(container, control_in.action)
+    if callable(action):
+        action()
+        return control_in
+    else:
+        return ControlOut(**control_in.dict(), message=action)
+
+
+@app.get(
+    "/status/",
+    response_model=Status,
+)
 def read_status():
     """Return whether the server is running or not."""
-    return Status(status=docker.containers.get(MC_SERVER_NAME).status)
+    control = ControlIn()
+    control.action = Actions.status
+    return create_control(control)
